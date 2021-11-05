45d00fdc011fabc88c7fb8f041df73c40187a4ca
Vico HP <victormu497@gmail.com>
2021-11-04 21:52:58 -0600

    Primer solución para evitar que el agente quede atrapado, moviendo un paso atrás

--- A_Espia.py
+++ A_Espia.py
@@ -2,6 +2,7 @@ import random
 from time import sleep
 from os import system, name
 
+
 def clear():
     # for windows
     if name == 'nt':
@@ -11,14 +12,14 @@ def clear():
         _ = system('clear')
 
 
-class A_Espia():
-    #Estados:
-        #0 | libre         | ' '
-        #1 | obstáculo     |  + or '|' or '='
-        #2 | ya explorado  |
-        #3 | guardia       |  G
-        #4 | tesoro        |  $
-        #5 | cárcel        |  C
+class A_Espia:
+    # Estados:
+    # 0 | libre         | ' '
+    # 1 | obstáculo     |  + or '|' or '='
+    # 2 | ya explorado  |
+    # 3 | guardia       |  G
+    # 4 | tesoro        |  $
+    # 5 | cárcel        |  C
     def __init__(self, originalMap):
         self.originalMap = tuple(originalMap)
         self.workingMap = originalMap
@@ -26,10 +27,14 @@ class A_Espia():
 
         # Almacena los estados en las 4 direcciones (arriba, derecha, abajo, izquierda)
         self.environment = [0, 0, 0, 0]
-        # Almacena la posición del agente y,x
-        self.position = [0, 1]
         # Almacena la posición donde entró el agente y,x
         self.startPosition = [0, 1]
+        # Almacena la posición del agente y,x
+        self.position = [0, 1]
+        # Almacena la posición anterior del agente y,x
+        self.pastPosition = [0, 1]
+        # Almacena el último movimiento realizado, en caso de tener que regresar puede usar esta variable. Str vacío porque no se puede vacía
+        self.lastMovement = ''
         # Bandera para saber si tiene el artefacto/tesoro o no
         self.hasTreasure = False
         # Caracteres por los que se puede mover, cuando tiene el artefacto solo puede moverse por el camino que ya
@@ -90,13 +95,15 @@ class A_Espia():
         if not self.hasTreasure:
             self.updateAgentMap()
 
+        isBounded = True
+
         for index in range(4):
             element = self.environment[index]
             if element == self.allowedPath:
                 self.environment[index] = 0
             elif element == '+' or element == '|' or element == '=':
                 self.environment[index] = 1
-            elif element == self.blockedPath:   #Falta analizar
+            elif element == self.blockedPath:  # Falta analizar
                 self.environment[index] = 2
             elif element == 'G':
                 self.environment[index] = 3
@@ -108,12 +115,30 @@ class A_Espia():
             else:
                 print('Revisa tus caracteres')
 
-        #self.testEnv()
-        return -1
+            #Si esta posición está en cero significa que hay camino por seguir
+                #La expreción es True y entramos para settear la variable a False
+            if isBounded and (self.environment[index] == 0):
+                isBounded = False
+
+        # self.testEnv()
+        if isBounded:
+            return -2 #Está encerrado y debe regresar un turno para retomar el curso
+        else:
+            return -1 #Puede moverse y tomará un camino aleatorio
+
+    def agentBounded(self):
+        if self.lastMovement == 0:
+            return 2
+        elif self.lastMovement == 1:
+            return 3
+        elif self.lastMovement == 2:
+            return 0
+        elif self.lastMovement == 3:
+            return 1
 
     def clearPosition(self):
+        self.pastPosition = self.position
         self.workingMap[self.position[0]][self.position[1]] = self.blockedPath
-        pass
 
     def updatePosition(self):
         self.workingMap[self.position[0]][self.position[1]] = 'E'
@@ -143,14 +168,21 @@ class A_Espia():
         willGetTreasure = False
 
         nextMove = self.checkEnvironment()
-        if nextMove != -1:
+        if nextMove > -1:
             impossibleMove = False
             willGetTreasure = True
+
+        # TODO What happens when spy gets blocked by his path and not even going back is enough?
+        if nextMove == -2:
+            impossibleMove = False
+            nextMove = self.agentBounded()
 
         while impossibleMove:
             nextMove = self.randNum()
             if self.environment[nextMove] == 0:
                 impossibleMove = False
+
+        self.lastMovement = nextMove
 
         if nextMove == 0:
             self.moveUp()
